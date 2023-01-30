530d5d8f0dc66ae34ce61de39eba9713466a149b
qxan <mehmet.enfa@gmail.com>
2023-01-30 18:31:50 +0300

    image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text className='text-red-500'>HomeScreen</Text>
-      <Text className='text-red-500'>HomeScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default HomeScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="mt-8">
+      <Text className="text-red-600">
+        {/* Header */}
+        <View>
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
