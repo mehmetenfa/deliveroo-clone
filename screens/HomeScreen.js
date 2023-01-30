d1bae163355f8376ecc73d6d4b1d705bbf64452b
qxan <mehmet.enfa@gmail.com>
2023-01-30 17:50:44 +0300

    tailwind install

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import React from 'react'
 const HomeScreen = () => {
   return (
     <View>
-      <Text className='text-red-500'>HomeScreen</Text>
+      <Text className='bg-red-500 font-bold'>HomeScreen</Text>
     </View>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./screens/**/*.{js.ts.jsx.tsx}",
-    "./pages/**/*.{js.ts.jsx.tsx}",
-    "./components/**/*.{js.ts.jsx.tsx}",
-  ],
-  // ...
-};
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
