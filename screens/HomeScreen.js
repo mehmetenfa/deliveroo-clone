f3df222e98d5fab5bac1b5a5fd831bff9dfda188
qxan <mehmet.enfa@gmail.com>
2023-01-31 14:04:02 +0300

    install heroicons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon } from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -15,19 +16,21 @@ const HomeScreen = () => {
     <SafeAreaView className="mt-8">
       <Text className="text-red-600">
         {/* Header */}
-        <View>
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image
             source={{
               uri: "https://links.papareact.com/wru",
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-        <View>
-            <Text>Deliver Now!</Text>
-            <Text>Current Location</Text>
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className='font-bold text-xl'>Current Location</Text>
+            <ChevronDownIcon size={20} color='#00CCBB' />
+          </View>
         </View>
-        </View>
-
       </Text>
     </SafeAreaView>
   );
